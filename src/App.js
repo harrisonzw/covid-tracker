3cc46c52328fd7996f6c99235b0d54d49f4f69b4
Harrison Wood <hwood22@ford.com>
2020-08-23 15:50:21 -0400

    Adding Countries selector

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
+import { FormControl, Select, MenuItem } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then(response => response.json())
+        .then(data => {
+          const countries = data.map(country => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">COVID19-tracker</header>
+      <div className="app-header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className="app-dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map(country => (
+              <MenuItem value="worldwide">{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
