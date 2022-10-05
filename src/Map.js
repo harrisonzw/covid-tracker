862cd5277ac77ad8c41113de7cebd17793646811
HWOOD22 <hwood22@ford.com>
2022-10-04 22:10:46 -0400

    Changed color of tip

--- src/Map.js
+++ src/Map.js
@@ -18,7 +18,7 @@ export default function Map({ countries, casesType, center, zoom }) {
         />
         {showDataOnMap(countries, casesType)}
       </LeafletMap>
-      <h5> Click on each circle to view country details </h5>
+      <h5 className='tip'> Click on each circle to view country details </h5>
     </div>
   );
 }
